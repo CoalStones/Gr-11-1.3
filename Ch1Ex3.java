c6c5120de09e5b6363a00f2171c2c62522c0dd0a
CoalStones <123333323+CoalStones@users.noreply.github.com>
2023-01-22 17:08:09 -0500

    Add files via upload

---
+++ Ch1Ex3.java
@@ -0,0 +1,18 @@
+
+package ch1ex3;
+
+
+public class Ch1Ex3 {
+
+
+    public static void main(String[] args) {
+        System.out.format("%-5s %-5s %-5s %-5s %-5s\n", " B", " I", " N", " G", " O");
+	System.out.format("%-5s %-5s %-5s %-5s %-5s\n", " 2", "20", "42", "60", "64");
+	System.out.format("%-5s %-5s %-5s %-5s %-5s\n", "14", "25", "32", "55", "70");
+	System.out.format("%-5s %-5s %-5s %-5s %-5s\n", " 5", "18", "FREE", "53", "67");
+	System.out.format("%-5s %-5s %-5s %-5s %-5s\n", "12", "16", "31", "46", "75");
+	System.out.format("%-5s %-5s %-5s %-5s %-5s\n", "10", "22", "39", "59", "71");
+
+    }
+    
+}
